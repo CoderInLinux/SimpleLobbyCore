a37f92bc1ba4a52299d5f62c8112344b752b032d
CoderInLinux <eli10077YT@gmail.com>
2024-07-31 11:48:57 -0600

    Updatings of the plugin

---
+++ src/main/java/org/coderinlinux/practicee/commands/FlyCommand.java
@@ -0,0 +1,17 @@
+package org.coderinlinux.practicee.commands;
+
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.command.Command;
+
+public class SpawnCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if(sender instanceof Player) {
+            if (cmd.ge)
+        }
+        return false;
+    }
+}
